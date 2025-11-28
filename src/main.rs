da86f99551fe6ee474a37cfd4c44506e63f81c6b
Albin <albin@malmo.skaggbyran.se>
2025-11-28 10:29:10 +0100

    UI start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use iced::Element;
+use iced::Element;
+use iced::widget::text_input;
+
+#[derive(Default)]
+struct State {
+    content: String,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ContentChanged(String),
+}
+
+pub fn main() -> iced::Result {
+    iced::run("amp", update, view)
+}
+
+fn view(state: &State) -> Element<'_, Message> {
+    text_input("Lägg till adress...", &state.content)
+        .on_input(Message::ContentChanged)
+        .into()
+}
+
+fn update(state: &mut State, message: Message) {
+    match message {
+        Message::ContentChanged(content) => {
+            state.content = content;
+        }
+    }
 }
