12ce42bb9910253b01582ee576749f53c687c3a9
Albin Sjögren <albin@sjoegren.se>
2026-01-29 16:37:21 +0100

    feat: add animated SVG background with radial gradients to topbar title

--- android/src/ui/topbar.rs
+++ android/src/ui/topbar.rs
@@ -36,6 +36,165 @@ pub fn TopBar(mut on_add_address: EventHandler<(String, String, String)>) -> Ele
     rsx! {
         div { class: "category-container topbar-container",
             div { class: "category-title topbar-title",
+                div { class: "topbar-bg-wrap",
+                    svg {
+                        xmlns: "http://www.w3.org/2000/svg",
+                        view_box: "0 0 100 100",
+                        "preserve-aspect-ratio": "xMidYMid slice",
+                        defs {
+                            "radial-gradient" {
+                                id: "Gradient1",
+                                cx: "50%",
+                                cy: "50%",
+                                fx: "0.441602%",
+                                fy: "50%",
+                                r: ".5",
+                                animate {
+                                    "attribute-name": "fx",
+                                    dur: "34s",
+                                    values: "0%;3%;0%",
+                                    "repeat-count": "indefinite",
+                                }
+                                stop {
+                                    offset: "0%",
+                                    "stop-color": "rgba(255, 0, 255, 1)",
+                                }
+                                stop {
+                                    offset: "100%",
+                                    "stop-color": "rgba(255, 0, 255, 0)",
+                                }
+                            }
+                            "radial-gradient" {
+                                id: "Gradient2",
+                                cx: "50%",
+                                cy: "50%",
+                                fx: "2.68147%",
+                                fy: "50%",
+                                r: ".5",
+                                animate {
+                                    "attribute-name": "fx",
+                                    dur: "23.5s",
+                                    values: "0%;3%;0%",
+                                    "repeat-count": "indefinite",
+                                }
+                                stop {
+                                    offset: "0%",
+                                    "stop-color": "rgba(255, 255, 0, 1)",
+                                }
+                                stop {
+                                    offset: "100%",
+                                    "stop-color": "rgba(255, 255, 0, 0)",
+                                }
+                            }
+                            "radial-gradient" {
+                                id: "Gradient3",
+                                cx: "50%",
+                                cy: "50%",
+                                fx: "0.836536%",
+                                fy: "50%",
+                                r: ".5",
+                                animate {
+                                    "attribute-name": "fx",
+                                    dur: "21.5s",
+                                    values: "0%;3%;0%",
+                                    "repeat-count": "indefinite",
+                                }
+                                stop {
+                                    offset: "0%",
+                                    "stop-color": "rgba(0, 255, 255, 1)",
+                                }
+                                stop {
+                                    offset: "100%",
+                                    "stop-color": "rgba(0, 255, 255, 0)",
+                                }
+                            }
+                        }
+                        rect {
+                            x: "13.744%",
+                            y: "1.18473%",
+                            width: "100%",
+                            height: "100%",
+                            fill: "url(#Gradient1)",
+                            transform: "rotate(334.41 50 50)",
+                            animate {
+                                "attribute-name": "x",
+                                dur: "20s",
+                                values: "25%;0%;25%",
+                                "repeat-count": "indefinite",
+                            }
+                            animate {
+                                "attribute-name": "y",
+                                dur: "21s",
+                                values: "0%;25%;0%",
+                                "repeat-count": "indefinite",
+                            }
+                            "animate-transform" {
+                                "attribute-name": "transform",
+                                "type": "rotate",
+                                from: "0 50 50",
+                                to: "360 50 50",
+                                dur: "7s",
+                                "repeat-count": "indefinite",
+                            }
+                        }
+                        rect {
+                            x: "-2.17916%",
+                            y: "35.4267%",
+                            width: "100%",
+                            height: "100%",
+                            fill: "url(#Gradient2)",
+                            transform: "rotate(255.072 50 50)",
+                            animate {
+                                "attribute-name": "x",
+                                dur: "23s",
+                                values: "-25%;0%;-25%",
+                                "repeat-count": "indefinite",
+                            }
+                            animate {
+                                "attribute-name": "y",
+                                dur: "24s",
+                                values: "0%;50%;0%",
+                                "repeat-count": "indefinite",
+                            }
+                            "animate-transform" {
+                                "attribute-name": "transform",
+                                "type": "rotate",
+                                from: "0 50 50",
+                                to: "360 50 50",
+                                dur: "12s",
+                                "repeat-count": "indefinite",
+                            }
+                        }
+                        rect {
+                            x: "9.00483%",
+                            y: "14.5733%",
+                            width: "100%",
+                            height: "100%",
+                            fill: "url(#Gradient3)",
+                            transform: "rotate(139.903 50 50)",
+                            animate {
+                                "attribute-name": "x",
+                                dur: "25s",
+                                values: "0%;25%;0%",
+                                "repeat-count": "indefinite",
+                            }
+                            animate {
+                                "attribute-name": "y",
+                                dur: "12s",
+                                values: "0%;25%;0%",
+                                "repeat-count": "indefinite",
+                            }
+                            "animate-transform" {
+                                "attribute-name": "transform",
+                                "type": "rotate",
+                                from: "360 50 50",
+                                to: "0 50 50",
+                                dur: "9s",
+                                "repeat-count": "indefinite",
+                            }
+                        }
+                    }
+                }
                 div { class: "topbar-title-content",
                     span { class: "topbar-title-text", "amp" }
                     button {
