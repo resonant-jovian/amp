9c25ce78854c5049e268b7ed64051dc1d2f6f6f6
Albin Sjögren <albin@sjoegren.se>
2026-01-30 11:41:24 +0100

    Add information icons to address fields

--- ios/src/ui/adresser.rs
+++ ios/src/ui/adresser.rs
@@ -1,10 +1,36 @@
 use dioxus::prelude::*;
+use dioxus_free_icons::icons::fa_solid_icons::FaCircleInfo;
+use dioxus_free_icons::Icon;
+
 #[component]
 pub fn Adresser() -> Element {
     rsx! {
         div { class: "stored-addresses",
             h2 { "Adresser" }
-            div { id: "addressList" }
+            div { id: "addressList",
+                // Example address items with info icons
+                div { class: "address-item",
+                    div { class: "address-text",
+                        button {
+                            class: "address-info-icon",
+                            onclick: move |_| (),
+                            Icon {
+                                icon: FaCircleInfo,
+                                width: 16,
+                                height: 16,
+                            }
+                        }
+                        span { "Example Address 1" }
+                    }
+                    div { class: "address-actions",
+                        label { class: "switch",
+                            input { r#type: "checkbox" }
+                            span { class: "slider" }
+                        }
+                        button { class: "btn-remove", onclick: move |_| (), "×" }
+                    }
+                }
+            }
         }
     }
 }
